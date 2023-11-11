28f81dc23d7bd4a7b1f2036adcaa353aa102cccc
Asura0666 <dhirajlande18@gmail.com>
2023-11-11 17:30:38 +0530

    30% work

--- prac-6.c
+++ prac-6.c
@@ -1,78 +1,67 @@
+
+
 #include <stdio.h>
 
-void bubbleSort(int arr[], int n) {
-    int temp, swapped;
-    do {
-        swapped = 0;
-        for (int i = 0; i < n - 1; i++) {
-            if (arr[i] > arr[i + 1]) {
-                temp = arr[i];
-                arr[i] = arr[i + 1];
-                arr[i + 1] = temp;
-                swapped = 1;
-            }
-        }
-    } while (swapped);
-}
 
-void selectionSort(int arr[], int n) {
-    int i, j, minIndex, temp;
-    for (i = 0; i < n - 1; i++) {
-        minIndex = i;
-        for (j = i + 1; j < n; j++) {
-            if (arr[j] < arr[minIndex]) {
-                minIndex = j;
-            }
+void insertionSort(int arr[], int n) {
+    int key;
+    for(int i=1; i<n; i++) {
+        key = arr[i];
+        int j=i-1;
+        while(j>=0 && arr[j] > key) {
+            arr[j+1] = arr[j];
+            j=j-1;
         }
-        temp = arr[i];
-        arr[i] = arr[minIndex];
-        arr[minIndex] = temp;
+        arr[j+1] = key;
     }
 }
 
-void insertionSort(int arr[], int n) {
-    int i, key, j;
-    for (i = 1; i < n; i++) {
-        key = arr[i];
-        j = i - 1;
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j = j - 1;
+void bubbleSort(int arr[], int n) {
+    for(int i=0; i<n-1; i++) {
+        for(int j=0; j<n-i-1; j++) {
+            if(arr[j] > arr[j+1]) {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
         }
-        arr[j + 1] = key;
+    }
+}
+
+void selectionSort(int arr[], int n) {
+    for(int i=0; i<n-1; i++) {
+        int minIndex = i;
+        for(int j=i+1; j<n; j++) {
+            if(arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        if(minIndex != i) {
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
     }
 }
 
 int main() {
     int n;
-
-    printf("Enter the number of elements: ");
-    scanf("%d", &n);
-
+    printf("\nEnter the Number of Elements: ");
+    scanf("%d",&n);
+    
     int arr[n];
-
-    printf("Enter %d elements:\n", n);
-    for (int i = 0; i < n; i++) {
+    printf("\nEnter The Number: ");
+    for(int i=0; i<n; i++) {
         scanf("%d", &arr[i]);
     }
-
-    insertionSort(arr, n);
-    printf("Sorted array in insertionSort order:\n");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
+    
+    // insertionSort(arr,n);
+    // bubbleSort(arr,n);
+    selectionSort(arr,n);
+    printf("\nSorted Array: ");
+    for(int i=0; i<n; i++) {
+        printf("%d ",arr[i]);
     }
-    printf("\n");
-    selectionSort(arr, n);
-    printf("Sorted array in selectionSort order:\n");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-    bubbleSort(arr, n);
-    printf("Sorted array in bubbleSort order:\n");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-
+    
     return 0;
 }

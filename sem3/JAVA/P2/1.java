e3d90c19933169d8ffe233fcfbc6aee9e522fbce
Asura0666 <dhirajlande18@gmail.com>
2024-09-23 23:59:59 +0530

    Completed

---
+++ sem3/JAVA/P2/1.java
@@ -0,0 +1,24 @@
+interface Drawable {
+  void Draw();
+}
+
+class rectangle implements Drawable {
+  public void Draw() {
+    System.out.println("Drawing Rectangle");
+  }
+}
+
+class circle implements Drawable {
+  public void Draw() {
+    System.out.println("Drawing Circle");
+  }
+}
+
+class InterfaceTest {
+  public static void main(String args[]) {
+    Drawable c = new circle();
+    c.Draw();
+    Drawable r = new rectangle();
+    r.Draw();
+  }
+}
